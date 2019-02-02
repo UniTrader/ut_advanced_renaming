a810460341d5221b995e32a9143972d9bf0a8c36
UniTrader <unitrader@gmx.de>
2019-02-02 16:26:54 +0100

    ..

--- utrenaming.lua
+++ utrenaming.lua
@@ -1,7 +1,384 @@
--- ffi setup
+-- stuff copied from the original file required for function
 local ffi = require("ffi")
 local C = ffi.C
 local utf8 = require("utf8")
+local config = {
+	mainFrameLayer = 5,
+	infoFrameLayer = 4,
+	contextFrameLayer = 3,
+
+	complexOrderParams = {
+		["trade"] = {
+			[1] = { id = "trade_location", name = ReadText(1001, 2943), type = "object", inputparams = { class = "sector" }, value = function (data) return next(data) and GetComponentData(data.station, "zoneid") or nil end },
+			[2] = { id = "trade_partner", name = ReadText(1001, 23), type = "object", inputparams = { class = "container" }, value = function (data) return data.station end },
+			[3] = { id = "trade_ware", name = ReadText(1001, 7104), type = "trade_ware", value = function (data) return next(data) and {data.isbuyoffer, data.ware} or nil end },
+			[4] = { id = "trade_amount", name = ReadText(1001, 6521), type = "trade_amount", value = function (data) return {data.desiredamount, data.amount} end },
+			data = function (value) return (value and menu.isInfoModeValidFor(menu.infoSubmenuObject, "orderqueue") and GetTradeData(value, ConvertStringTo64Bit(tostring(menu.infoSubmenuObject)))) or {} end
+		}
+	},
+	moduletypes = {
+		{ type = "moduletypes_production", name = ReadText(1001, 2421) },
+		{ type = "moduletypes_build",      name = ReadText(1001, 2439) },
+		{ type = "moduletypes_storage",    name = ReadText(1001, 2422) },
+		{ type = "moduletypes_habitation", name = ReadText(1001, 2451) },
+		{ type = "moduletypes_dock",       name = ReadText(1001, 2452) },
+		{ type = "moduletypes_defence",    name = ReadText(1001, 2424) },
+		{ type = "moduletypes_other",      name = ReadText(1001, 2453) },
+		{ type = "moduletypes_venture",    name = ReadText(1001, 2454) },
+	},
+	stateKeys = {
+		{"mode"},
+		{"modeparam"},
+		{"lastactivetable"},
+		{"focuscomponent", "UniverseID"},
+		{"currentsector", "UniverseID"},
+		{"orderQueueMode"},
+		{"selectedcomponents"},
+		{"searchtext"},
+		{"selectedorder"},
+		{"infoTableMode"},
+		{"infoSubmenuObject", "UniverseID"},
+	},
+	leftBar = {
+		{ name = ReadText(1001, 3224),	icon = "mapst_objectlist",			mode = "objectlist" },
+		{ name = ReadText(1001, 1000),	icon = "mapst_propertyowned",		mode = "propertyowned" },
+		{ spacing = true },
+		{ name = ReadText(1001, 3324),	icon = "mapst_mission_offers",		mode = "missionoffer" },
+		{ name = ReadText(1001, 3323),	icon = "mapst_mission_accepted",	mode = "mission" },
+		{ spacing = true },
+		{ name = ReadText(1001, 2427),	icon = "mapst_information",			mode = "info" },
+		{ spacing = true },
+		{ name = ReadText(1001, 3226),	icon = "mapst_plotmanagement",		mode = "plots" },
+		{ spacing = true,																			condition = IsCheatVersion }, -- (cheats only)
+		{ name = "Cheats",				icon = "mapst_cheats",				mode = "cheats",		condition = IsCheatVersion }, -- (cheats only)
+	},
+	rightBar = {
+		{ name = ReadText(1001, 3227),	icon = "mapst_filtersystem",		mode = "filter" },
+		{ name = ReadText(1001, 9801),	icon = "mapst_legend",				mode = "legend" },
+	},
+	infoCategories = {
+		{ category = "objectinfo",				name = ReadText(1001, 2427),	icon = "mapst_information" },
+		{ category = "orderqueue",				name = ReadText(1001, 8360),	icon = "mapst_ao_orderqueue" },
+		{ category = "orderqueue_advanced",		name = ReadText(1001, 8361),	icon = "mapst_orderqueue_advanced" },
+	},
+	layers = {
+		{ name = ReadText(1001, 3252),	icon = "mapst_fs_trade",		mode = "layer_trade" },
+		{ name = ReadText(1001, 3253),	icon = "mapst_fs_think",		mode = "layer_think" },
+		{ name = ReadText(1001, 8329),	icon = "mapst_fs_mining",		mode = "layer_mining" },
+		{ name = ReadText(1001, 3254),	icon = "mapst_fs_other",		mode = "layer_other" },
+	},
+	layersettings = {
+		["layer_trade"] = {
+			callback = function (value) return C.SetMapRenderTradeOffers(menu.holomap, value) end,
+			[1] = {
+				caption = ReadText(1001,46),
+				info = ReadText(1001,3279),
+				overrideText = ReadText(1001,3278),
+				type = "dropdownlist",
+				id = "trade_wares",
+				callback = function (...) return menu.filterTradeWares(...) end,
+				listOptions = function (...) return menu.getFilterTradeWaresOptions(...) end,
+			},
+			[2] = {
+				caption = ReadText(1001,1400),
+				type = "checkbox",
+				callback = function (...) return menu.filterTradeStorage(...) end,
+				[1] = {
+					id = "trade_storage_container",
+					name = ReadText(20205,100),
+					info = ReadText(1001,3280),
+					param = "container",
+				},
+				[2] = {
+					id = "trade_storage_solid",
+					name = ReadText(20205,200),
+					info = ReadText(1001,3281),
+					param = "solid",
+				},
+				[3] = {
+					id = "trade_storage_liquid",
+					name = ReadText(20205,300),
+					info = ReadText(1001,3282),
+					param = "liquid",
+				},
+			},
+			[3] = {
+				caption = ReadText(1001,2808),
+				type = "slidercell",
+				callback = function (...) return menu.filterTradePrice(...) end,
+				[1] = {
+					id = "trade_price_maxprice",
+					name = ReadText(1001,3284),
+					info = ReadText(1001,3283),
+					param = "maxprice",
+					scale = {
+						min       = 0,
+						max       = 5000,
+						step      = 1,
+						suffix    = ReadText(1001, 101),
+						exceedmax = true
+					}
+				},
+			},
+			[4] = {
+				caption = ReadText(1001, 8357),
+				type = "dropdown",
+				callback = function (...) return menu.filterTradeVolume(...) end,
+				[1] = {
+					id = "trade_volume",
+					info = ReadText(1001, 8358),
+					listOptions = function (...) return menu.getFilterTradeVolumeOptions(...) end,
+					param = "volume"
+				},
+			},
+			[5] = {
+				caption = ReadText(1001, 8343),
+				type = "slidercell",
+				callback = function (...) return menu.filterTradeOffer(...) end,
+				[1] = {
+					id = "trade_offer_number",
+					name = ReadText(1001, 8344),
+					info = ReadText(1001, 8345),
+					param = "number",
+					scale = {
+						min       = 0,
+						minSelect = 1,
+						max       = 5,
+						step      = 1,
+						exceedmax = true
+					}
+				},
+			},
+		},
+		["layer_fight"] = {},
+		["layer_think"] = {
+			callback = function (value) return menu.filterThink(value) end,
+			[1] = {
+				caption = ReadText(1001,3285),
+				type = "dropdown",
+				callback = function (...) return menu.filterThinkAlert(...) end,
+				[1] = {
+					info = ReadText(1001,3286),
+					id = "think_alert",
+					listOptions = function (...) return menu.getFilterThinkAlertOptions(...) end,
+					param = "alert"
+				},
+			},
+		},
+		["layer_build"] = {},
+		["layer_diplo"] = {},
+		["layer_mining"] = {
+			callback = function (value) return menu.filterMining(value) end,
+			[1] = {
+				caption = ReadText(1001, 8330),
+				type = "checkbox",
+				callback = function (...) return menu.filterMiningResources(...) end,
+				[1] = {
+					id = "mining_resource_display",
+					name = ReadText(1001, 8331),
+					info = ReadText(1001, 8332),
+					param = "display"
+				},
+			},
+		},
+		["layer_other"] = {
+			callback = function (value) return menu.filterOther(value) end,
+			[1] = {
+				caption = ReadText(1001, 8335),
+				type = "checkbox",
+				callback = function (...) return menu.filterOtherStation(...) end,
+				[1] = {
+					id = "other_misc_missions",
+					name = ReadText(1001, 3291),
+					info = ReadText(1001, 3292),
+					param = "missions",
+				},
+				[2] = {
+					id = "other_misc_cargo",
+					name = ReadText(1001, 3289),
+					info = ReadText(1001, 3290),
+					param = "cargo",
+				},
+				[3] = {
+					id = "other_misc_workforce",
+					name = ReadText(1001, 3293),
+					info = ReadText(1001, 3294),
+					param = "workforce",
+				},
+				[4] = {
+					id = "other_misc_dockedships",
+					name = ReadText(1001, 3275),
+					info = ReadText(1001, 3299),
+					param = "dockedships",
+				},
+				[5] = {
+					id = "other_misc_civilian",
+					name = ReadText(1001, 8333),
+					info = ReadText(1001, 8334),
+					param = "civilian",
+				},
+			},
+			[2] = {
+				caption = ReadText(1001, 8336),
+				type = "checkbox",
+				callback = function (...) return menu.filterOtherShip(...) end,
+				[1] = {
+					id = "other_misc_orderqueue",
+					name = ReadText(1001, 3287),
+					info = ReadText(1001, 8372),
+					param = "orderqueue",
+				},
+				[2] = {
+					id = "other_misc_allyorderqueue",
+					name = ReadText(1001, 8370),
+					info = ReadText(1001, 8371),
+					param = "allyorderqueue",
+				},
+				[3] = {
+					id = "other_misc_crew",
+					name = ReadText(1001, 3295),
+					info = ReadText(1001, 3296),
+					param = "crew",
+				},
+			},
+			[3] = {
+				caption = ReadText(1001, 2664),
+				type = "checkbox",
+				callback = function (...) return menu.filterOtherMisc(...) end,
+				[1] = {
+					id = "other_misc_ecliptic",
+					name = ReadText(1001, 3297),
+					info = ReadText(1001, 3298),
+					param = "ecliptic",
+				},
+			},
+		}
+	},
+	mapfilterversion = 11,
+
+	-- custom default row properties, different from Helper defaults
+	mapRowHeight = Helper.standardTextHeight,
+	mapFontSize = Helper.standardFontSize,
+	plotPairedDimension = { posX = "negX", negX = "posX", posY = "negY", negY = "posY", posZ = "negZ", negZ = "posZ" },
+	maxPlotSize = 20,
+
+	contextBorder = 5,
+
+	classOrder = {
+		["station"]		= 1,
+		["ship_xl"]		= 2,
+		["ship_l"]		= 3,
+		["ship_m"]		= 4,
+		["ship_s"]		= 5,
+		["ship_xs"]		= 6,
+	},
+	purposeOrder = {
+		["fight"]	= 1,
+		["build"]	= 2,
+		["mine"]	= 3,
+		["trade"]	= 4,
+	},
+
+	missionMainTypeOrder = {
+		["plot"] = 1,
+		["tutorial"] = 2,
+		["generic"] = 3,
+		["upkeep"] = 4,
+		["guidance"] = 5,
+	},
+
+	missionOfferCategories = {
+		{ category = "guild",	name = ReadText(1001, 3331),	icon = "mapst_mission_guild" },
+		{ category = "other",	name = ReadText(1001, 3332),	icon = "mapst_mission_other" },
+	},
+
+	missionCategories = {
+		{ category = "guild",		name = ReadText(1001, 3333),	icon = "mapst_mission_guild" },
+		{ category = "other",		name = ReadText(1001, 3334),	icon = "mapst_mission_other" },
+		{ category = "upkeep",		name = ReadText(1001, 3305),	icon = "mapst_mission_upkeep" },
+		{ category = "guidance",	name = ReadText(1001, 3329),	icon = "mapst_mission_guidance" },
+	},
+
+	autopilotmarker = ">> ",
+	softtargetmarker_l = "> ",
+
+	tradeContextMenuWidth = 0.5 * Helper.viewWidth + Helper.scrollbarWidth,
+
+	legend = {
+		-- hexes
+		{ icon = "maplegend_hexagon_fog_01",		text = ReadText(10002, 606),	width = Helper.sidebarWidth,	height = Helper.sidebarWidth },														-- Unknown location
+		{ icon = "maplegend_hexagon_01",			text = ReadText(1001, 9806),	width = Helper.sidebarWidth,	height = Helper.sidebarWidth,	color = { r = 255, g = 0, b = 0, a = 100 } },		-- Mineral Region
+		{ icon = "maplegend_hexagon_01",			text = ReadText(1001, 9807),	width = Helper.sidebarWidth,	height = Helper.sidebarWidth,	color = { r = 0, g = 0, b = 255, a = 100 }  },		-- Gas Region
+		{ icon = "maplegend_hexagon_01",			text = ReadText(1001, 9812),	width = Helper.sidebarWidth,	height = Helper.sidebarWidth,	color = { r = 255, g = 0, b = 255, a = 100 }  },	-- Mineral/Gas Region
+		-- highways, gates, etc
+		{ icon = "solid",							text = ReadText(1001, 9809),	width = Helper.sidebarWidth,	height = Helper.standardTextHeight / 2,	minRowHeight = Helper.sidebarWidth / 2 },	-- Jump Gate Connection
+		{ icon = "maplegend_hw_01",					text = ReadText(20001, 601),	width = Helper.sidebarWidth,	height = Helper.sidebarWidth / 2,	color = "superhighwaycolor" },					-- Superhighway
+		{ icon = "maplegend_hw_01",					text = ReadText(20001, 501),	width = Helper.sidebarWidth,	height = Helper.sidebarWidth / 2,	color = "highwaycolor" },						-- Local Highway
+		{ icon = "mapob_jumpgate",					text = ReadText(20001, 701),	color = "gatecolor" },			-- Jump Gate
+		{ icon = "mapob_transorbital_accelerator",	text = ReadText(20001, 1001),	color = "gatecolor" },			-- Accelarator
+		{ icon = "mapob_superhighway",				text = ReadText(1001, 9810),	color = "highwaygatecolor" },	-- Superhighway Gate
+		{ icon = "ship_s_fight_01",					text = ReadText(1001, 5200),	color = "playercolor" },		-- Owned
+		{ icon = "ship_s_fight_01",					text = ReadText(1001, 5202),	color = "friendcolor" },		-- Neutral
+		{ icon = "ship_s_fight_01",					text = ReadText(1001, 5201),	color = "enemycolor" },			-- Enemy
+		-- stations
+		{ text = ReadText(1001, 4) },																																					-- Stations
+		{ icon = "mapob_playerhq",					text = ReadText(20102, 2011),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "playercolor" },	-- Headquarters
+		{ icon = "maplegend_hq_01",					text = ReadText(1001, 9808),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Faction Headquarters
+		{ icon = "mapob_shipyard",					text = ReadText(1001, 92),		width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Shipyard
+		{ icon = "mapob_wharf",						text = ReadText(1001, 9805),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Wharf
+		{ icon = "mapob_equipmentdock",				text = ReadText(1001, 9804),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Equipment Dock
+		{ icon = "mapob_tradestation",				text = ReadText(1001, 9803),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Trading Station
+		{ icon = "mapob_defensestation",			text = ReadText(1001, 9802),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Defence Platform
+		{ icon = "mapob_piratestation",				text = ReadText(20102, 1511),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Free Port
+		{ icon = "mapob_factory",					text = ReadText(20102, 1001),	width = 0.8 * Helper.sidebarWidth,	height = 0.8 * Helper.sidebarWidth,	color = "friendcolor" },	-- Factory
+		-- xl ships
+		{ text = ReadText(1001, 6) .. ReadText(1001, 120) .. " " .. ReadText(20111, 5041) },					-- Ships: XL
+		{ icon = "ship_xl_fight_01",				text = ReadText(20221, 2011),	color = "friendcolor" },	-- Fighter
+		--{ icon = "ship_xl_trade_01",				text = ReadText(20221, 4011),	color = "friendcolor" },	-- Freighter
+		--{ icon = "ship_xl_mine_01",				text = ReadText(20221, 3041),	color = "friendcolor" },	-- Miner
+		{ icon = "ship_xl_build_01",				text = ReadText(20221, 5021),	color = "friendcolor" },	-- Builder
+		-- l ships
+		{ text = ReadText(1001, 6) .. ReadText(1001, 120) .. " " .. ReadText(20111, 5031) },					-- Ships: XL
+		{ icon = "ship_l_fight_01",					text = ReadText(20221, 2011),	color = "friendcolor" },	-- Fighter
+		{ icon = "ship_l_trade_01",					text = ReadText(20221, 4011),	color = "friendcolor" },	-- Freighter
+		{ icon = "ship_l_mine_01",					text = ReadText(20221, 3041),	color = "friendcolor" },	-- Miner
+		-- m ships
+		{ text = ReadText(1001, 6) .. ReadText(1001, 120) .. " " .. ReadText(20111, 5021) },					-- Ships: XL
+		{ icon = "ship_m_fight_01",					text = ReadText(20221, 2011),	color = "friendcolor" },	-- Fighter
+		{ icon = "ship_m_trade_01",					text = ReadText(20221, 3031),	color = "friendcolor" },	-- Transporter
+		{ icon = "ship_m_mine_01",					text = ReadText(20221, 3041),	color = "friendcolor" },	-- Miner
+		-- s ships
+		{ text = ReadText(1001, 6) .. ReadText(1001, 120) .. " " .. ReadText(20111, 5011) },					-- Ships: XL
+		{ icon = "ship_s_fight_01",					text = ReadText(20221, 2011),	color = "friendcolor" },	-- Fighter
+		{ icon = "ship_s_trade_01",					text = ReadText(20221, 3031),	color = "friendcolor" },	-- Transporter
+		{ icon = "ship_s_mine_01",					text = ReadText(20221, 3041),	color = "friendcolor" },	-- Miner
+		-- xs ships
+		{ text = ReadText(1001, 22) },																			-- Units
+		{ icon = "ship_xs_fight_01",				text = ReadText(20101, 100401),	color = "friendcolor" },	-- Defence Drone
+		{ icon = "ship_xs_trade_01",				text = ReadText(20101, 100101),	color = "friendcolor" },	-- Cargo Drone
+		{ icon = "ship_xs_mine_01",					text = ReadText(20101, 100501),	color = "friendcolor" },	-- Mining Drone
+		{ icon = "ship_xs_neutral_01",				text = ReadText(20101, 110201),	color = "friendcolor" },	-- Civilian Ship
+		{ icon = "ship_xs_build_01",				text = ReadText(20101, 100301),	color = "friendcolor" },	-- Building Drone
+		-- misc
+		{ text = ReadText(1001, 2664) },																								-- Misc
+		{ icon = "mapob_lasertower_xs",				text = ReadText(20201, 20501),	color = "friendcolor" },							-- Laser Tower Mk1
+		{ icon = "mapob_lasertower_s",				text = ReadText(20201, 20601),	color = "friendcolor" },							-- Laser Tower Mk2
+		{ icon = "mapob_mine",						text = ReadText(20201, 20201),	color = "friendcolor" },							-- Mine
+		{ icon = "solid",							text = ReadText(1001, 1304),	width = 4,	height = 4,	color = "missilecolor" },	-- Missiles
+		{ icon = "mapob_satellite_01",				text = ReadText(20201, 20301),	color = "friendcolor" },							-- Satellite
+		{ icon = "mapob_satellite_02",				text = ReadText(20201, 20401),	color = "friendcolor" },							-- Advanced Satellite
+		{ icon = "mapob_resourceprobe",				text = ReadText(20201, 20701),	color = "friendcolor" },							-- Resource Probe
+		{ icon = "mapob_navbeacon",					text = ReadText(20201, 20801),	color = "friendcolor" },							-- Nav Beacon
+		{ icon = "mapob_poi",						text = ReadText(1001, 9811),	color = "friendcolor" },							-- Point of Interest
+		{ icon = "mapob_unknown",					text = ReadText(20109, 5001) },														-- Unknown Object
+		{ icon = "npc_factionrep",					text = ReadText(20208, 10601),	color = "friendcolor" },							-- Faction Representative
+		{ icon = "npc_missionactor",				text = ReadText(30260, 1901),	color = "missioncolor" },							-- Person of Interest
+		{ icon = "npc_shadyguy",					text = ReadText(20208, 10801),	color = "friendcolor" },							-- Black Marketeer
+		{ icon = "missionoffer_fight_active",		text = ReadText(1001, 3291),	color = "missioncolor" },							-- Mission Offers
+		{ icon = "mapob_missiontarget",				text = ReadText(1001, 3325),	color = "missioncolor" },							-- Accepted Missions
+	},
+}
+
 
 
 local orig = {}
